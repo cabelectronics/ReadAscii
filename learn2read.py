745b2253d4018708e4793af131d058c9d78929ce
cabelectronics <67470879+cabelectronics@users.noreply.github.com>
2020-07-26 20:20:51 +0200

    Add files via upload

---
+++ learn2read.py
@@ -0,0 +1,373 @@
+print("La distancia a meta es de:")
+
+with open("learn2read.txt") as openfile:
+    for line in openfile:
+        for part in line.split():
+            if "32" in part:
+                print(" ")
+            else:
+                pass
+            if "33" in part:
+                print("!")
+            else:
+                pass
+            if "35" in part:
+                print("#")
+            else:
+                pass
+            if "36" in part:
+                print("$")
+            else:
+                pass
+            if "37" in part:
+                print("%")
+            else:
+                pass
+            if "38" in part:
+                print("&")
+            else:
+                pass
+            if "39" in part:
+                print("'")
+            else:
+                pass
+            if "40" in part:
+                print("(")
+            else:
+                pass
+            if "41" in part:
+                print(")")
+            else:
+                pass
+            if "42" in part:
+                print("*")
+            else:
+                pass
+            if "43" in part:
+                print("+")
+            else:
+                pass
+            if "44" in part:
+                print(",")
+            else:
+                pass
+            if "45" in part:
+                print("_")
+            else:
+                pass
+            if "46" in part:
+                print(".")
+            else:
+                pass
+            if "47" in part:
+                print("/")
+            else:
+                pass
+            if "48" in part:
+                print("0")
+            else:
+                pass
+            if "49" in part:
+                print("1")
+            else:
+                pass
+            if "50" in part:
+                print("2")
+            else:
+                pass
+            if "51" in part:
+                print("3")
+            else:
+                pass
+            if "52" in part:
+                print("4")
+            else:
+                pass
+            if "53" in part:
+                print("5")
+            else:
+                pass
+            if "54" in part:
+                print("6")
+            else:
+                pass
+            if "55" in part:
+                print("7")
+            else:
+                pass
+            if "56" in part:
+                print("8")
+            else:
+                pass
+            if "57" in part:
+                print("9")
+            else:
+                pass
+            if "58" in part:
+                print(":")
+            else:
+                pass
+            if "59" in part:
+                print(";")
+            else:
+                pass
+            if "60" in part:
+                print("<")
+            else:
+                pass
+            if "61" in part:
+                print("=")
+            else:
+                pass
+            if "62" in part:
+                print(">")
+            else:
+                pass
+            if "63" in part:
+                print("?")
+            else:
+                pass
+            if "65" in part:
+                print("A")
+            else:
+                pass
+            if "66" in part:
+                print("B")
+            else:
+                pass
+            if "67" in part:
+                print("C")
+            else:
+                pass
+            if "68" in part:
+                print("D")
+            else:
+                pass
+            if "69" in part:
+                print("E")
+            else:
+                pass
+            if "70" in part:
+                print("F")
+            else:
+                pass
+            if "71" in part:
+                print("G")
+            else:
+                pass
+            if "72" in part:
+                print("H")
+            else:
+                pass
+            if "73" in part:
+                print("I")
+            else:
+                pass
+            if "74" in part:
+                print("J")
+            else:
+                pass
+            if "75" in part:
+                print("K")
+            else:
+                pass
+            if "76" in part:
+                print("L")
+            else:
+                pass
+            if "77" in part:
+                print("M")
+            else:
+                pass
+            if "78" in part:
+                print("N")
+            else:
+                pass
+            if "79" in part:
+                print("O")
+            else:
+                pass
+            if "80" in part:
+                print("P")
+            else:
+                pass
+            if "81" in part:
+                print("Q")
+            else:
+                pass
+            if "82" in part:
+                print("R")
+            else:
+                pass
+            if "83" in part:
+                print("S")
+            else:
+                pass
+            if "84" in part:
+                print("T")
+            else:
+                pass
+            if "85" in part:
+                print("U")
+            else:
+                pass
+            if "86" in part:
+                print("V")
+            else:
+                pass
+            if "87" in part:
+                print("W")
+            else:
+                pass
+            if "88" in part:
+                print("X")
+            else:
+                pass
+            if "89" in part:
+                print("Y")
+            else:
+                pass
+            if "90" in part:
+                print("Z")
+            else:
+                pass
+            if "91" in part:
+                print("[")
+            else:
+                pass
+            if "93" in part:
+                print("]")
+            else:
+                pass
+            if "94" in part:
+                print("^")
+            else:
+                pass
+            if "95" in part:
+                print("_")
+            else:
+                pass
+            if "96" in part:
+                print("`")
+            else:
+                pass
+            if "97" in part:
+                print("a")
+            else:
+                pass
+            if "98" in part:
+                print("b")
+            else:
+                pass
+            if "99" in part:
+                print("c")
+            else:
+                pass
+            if "100" in part:
+                print("d")
+            else:
+                pass
+            if "101" in part:
+                print("e")
+            else:
+                pass
+            if "102" in part:
+                print("f")
+            else:
+                pass
+            if "103" in part:
+                print("g")
+            else:
+                pass
+            if "104" in part:
+                print("h")
+            else:
+                pass
+            if "105" in part:
+                print("i")
+            else:
+                pass
+            if "106" in part:
+                print("j")
+            else:
+                pass
+            if "107" in part:
+                print("k")
+            else:
+                pass
+            if "108" in part:
+                print("l")
+            else:
+                pass
+            if "109" in part:
+                print("m")
+            else:
+                pass
+            if "110" in part:
+                print("n")
+            else:
+                pass
+            if "111" in part:
+                print("o")
+            else:
+                pass
+            if "112" in part:
+                print("p")
+            else:
+                pass
+            if "113" in part:
+                print("q")
+            else:
+                pass
+            if "114" in part:
+                print("r")
+            else:
+                pass
+            if "115" in part:
+                print("s")
+            else:
+                pass
+            if "116" in part:
+                print("t")
+            else:
+                pass
+            if "117" in part:
+                print("u")
+            else:
+                pass
+            if "118" in part:
+                print("v")
+            else:
+                pass
+            if "119" in part:
+                print("w")
+            else:
+                pass
+            if "120" in part:
+                print("x")
+            else:
+                pass
+            if "121" in part:
+                print("y")
+            else:
+                pass
+            if "122" in part:
+                print("z")
+            else:
+                pass
+            if "123" in part:
+                print("{")
+            else:
+                pass
+            if "124" in part:
+                print("|")
+            else:
+                pass
+            if "125" in part:
+                print("}")
+            else:
+                pass
+            if "126" in part:
+                print("~")
+            else:
+                pass
